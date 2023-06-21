c4df8e0b4cd786e8075a4e3f35e270f1f7c0e18a
L0rd <headood007@gmail.com>
2023-06-21 14:24:47 +0300

    front-end and API refactor

--- express-api/Watch/watch.js
+++ express-api/Watch/watch.js
@@ -24,14 +24,15 @@ exports.watch = async (req, res, next) => {
 }
 
 exports.unwatch = async (req, res, next) => {
-  const { username, show_id, season_nr, episode_nr } = req.body
+  const params = {
+      username:req.query.username,
+      show_id:req.query.show_id,
+        season_nr:req.query.season_nr,
+        episode_nr:req.query.episode_nr
+    }
+    console.log(params)
   try {
-    await Watched.deleteOne({
-      username,
-      show_id,
-        season_nr,
-        episode_nr
-    }).then(watched =>
+    await Watched.deleteOne(params).then(watched =>
       res.status(200).json({
         message: "Watch record successfully deleted.",
         watched,
@@ -46,9 +47,22 @@ exports.unwatch = async (req, res, next) => {
 }
 
 exports.fetch = async (req, res, next) => {
-  const username = req.query.username
+  const searchParameters = {username:undefined,show_id:undefined}
+  if (req.query.username){
+    searchParameters["username"] = req.query.username
+  }
+  else {
+    delete searchParameters["username"]
+  }
+  if (req.query.show_id){
+    searchParameters["show_id"] = req.query.show_id
+  }
+  else {
+    delete searchParameters["show_id"]
+  }
+  console.log(searchParameters)
   try {
-    await Watched.find({username:username}).then(results =>
+    await Watched.find(searchParameters).then(results =>
       res.status(200).json({
         message: "Watch record successfully retrieved.",
         results,
@@ -56,7 +70,7 @@ exports.fetch = async (req, res, next) => {
     )
   } catch (err) {
     res.status(401).json({
-      message: "Unsuccessful",
+      message: "Unsuccessful.",
       error: error.mesage,
     })
   }
